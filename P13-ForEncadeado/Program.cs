b32230e588c91ca8d5b67dba79f6fe18d7b19171
Fernanda Ono <fernanda.ono@avanade.com>
2020-09-04 14:53:53 -0300

    for encadeado

---
+++ P13-ForEncadeado/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters;
+
+namespace P13_ForEncadeado
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Projeto 13");
+
+			// *
+			// **
+			// ***
+			// um for para cada linha e cada coluna
+
+			for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+			{
+				for (int contadorColuna = 0; contadorColuna < 10; contadorColuna++)
+				{
+					Console.Write("*");
+					if (contadorColuna >= contadorLinha)
+						break;
+				}
+				Console.WriteLine();
+			}
+
+			//Outra forma sem o break
+			for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+			{
+				for (int contadorColuna = 0; contadorColuna <= contadorLinha; contadorColuna++)
+				{
+					Console.Write("*");
+				}
+				Console.WriteLine();
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
